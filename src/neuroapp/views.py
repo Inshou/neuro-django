9779918cd93391dd8506688eeef31217d7dbb185
pchernyshev <pchernyshev@itkey.com>
2019-10-30 15:29:29 +0300

    init new project

--- src/neuroapp/views.py
+++ src/neuroapp/views.py
@@ -14,15 +14,15 @@ def index(request):
         form = UserForm(request.POST, request.FILES)  # Do not forget to add: request.FILES
         if form.is_valid():
             image_path = form.save().image.path
-            #evaluate = test_func(image_path)
+            evaluate = test_func(image_path)
             resultform = ResultForm(initial={"answer": "thumb_up"})
             #os.remove(image_path)
-            return HttpResponse("<h2>Результат проверки: {0}</h2>".format(image_path))
-            #return render(request, "result.html", {"my_text_1": "{0}: {1}%".format('thumb_up', round((evaluate['thumb_up']*100),3)),
-            #                                       "my_text_2": "{0}: {1}%".format('ok', round((evaluate['ok']*100),3)),
-            #                                       "my_path": "/media/"+os.path.basename(image_path),
-            #                                       "image_name": os.path.basename(image_path),
-            #                                       "form": resultform})
+            #return HttpResponse("<h2>Результат проверки: {0}</h2>".format(image_path))
+            return render(request, "result.html", {"my_text_1": "{0}: {1}%".format('thumb_up', round((evaluate['thumb_up']*100),3)),
+                                                   "my_text_2": "{0}: {1}%".format('ok', round((evaluate['ok']*100),3)),
+                                                   "my_path": "/media/"+os.path.basename(image_path),
+                                                   "image_name": os.path.basename(image_path),
+                                                   "form": resultform})
         return HttpResponse("<h2>Произошла ошибка {0}</h2>".format(""))
     else:
         userform = UserForm()
